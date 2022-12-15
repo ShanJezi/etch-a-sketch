3f86687fff6196ae20de1cfa5212b721b042bd40
Shan Jezi <sobrien1028@gmail.com>
2022-12-15 15:35:29 -0800

    makeGrid() works for 16x16 grid only

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const gridContainer = document.querySelector('.grid-container');
 
 function makeGrid(rows, cols) {
-  for (let i = 0; i <= (rows * cols); i++) {
+  for (let i = 0; i < (rows * cols); i++) {
     let newGridItem = document.createElement('div');
     newGridItem.classList.add('grid-item');
     newGridItem.textContent = (i);
     gridContainer.appendChild(newGridItem);
   };
-  gridContainer.setProperty('--grid-rows', rows);
-  gridContainer.setProperty('--grid-cols', cols);
 };
 
 makeGrid(16, 16);
