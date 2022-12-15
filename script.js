824a3f8092852053e6d3928b606d3372d2c8bbd9
Shan Jezi <sobrien1028@gmail.com>
2022-12-15 15:31:10 -0800

    makeGrid(rows, cols) makes divs, but not grid...

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+const gridContainer = document.querySelector('.grid-container');
+
+function makeGrid(rows, cols) {
+  for (let i = 0; i <= (rows * cols); i++) {
+    let newGridItem = document.createElement('div');
+    newGridItem.classList.add('grid-item');
+    newGridItem.textContent = (i);
+    gridContainer.appendChild(newGridItem);
+  };
+  gridContainer.setProperty('--grid-rows', rows);
+  gridContainer.setProperty('--grid-cols', cols);
+};
+
+makeGrid(16, 16);
