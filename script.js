ffcb792d5d63c2f0906280fb7c841435b270941d
Shan Jezi <sobrien1028@gmail.com>
2022-12-15 15:58:35 -0800

    make grid any size
    spacing is still funky between grid items

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridContainer = document.querySelector('.grid-container');
 
 function makeGrid(rows, cols) {
+  gridContainer.setAttribute('style', 
+  `grid-template-columns: repeat(${cols}, 1fr);`, 
+  `grid-template-rows: repeat(${rows}, 1fr);`)
   for (let i = 0; i < (rows * cols); i++) {
     let newGridItem = document.createElement('div');
     newGridItem.classList.add('grid-item');
